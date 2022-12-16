40796de636cff36e07af1d4093df940d6690748b
Raphael Braga <106981015+rbragadev@users.noreply.github.com>
2022-12-16 16:19:34 -0300

    editarDados

--- 8.dbo/index.js
+++ 8.dbo/index.js
@@ -2,13 +2,87 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mysql = require('mysql');
 
-const app = express;
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
 
 app.engine('handlebars', exphbs.engine());
-app.request('view engine', 'Handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.length('/', (req, res) => {
+app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    res.redirect('/');
+  });
+});
+
+app.get('/books', (req, res) => {
+  const sql = 'SELECT * FROM books';
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+
+    console.log(books);
+
+    res.render('books', { books });
+  });
+});
+
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id;
+
+  const query = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(query, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render('book', { book });
+  });
+});
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysql',
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Conectado ao MySQL!');
+
+  app.listen(3000);
 });
