ed128de62dd370a4a2394b4f49331c6aa26f16ee
Raphael Braga <106981015+rbragadev@users.noreply.github.com>
2022-12-19 18:10:32 -0300

    sequelize

--- 9.sequelize/index.js
+++ 9.sequelize/index.js
@@ -20,30 +20,77 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/users/create', (req, res) => {
+app.get('/', function (req, res) {
+  User.findAll({ raw: true })
+    .then((users) => {
+      console.log(users);
+      res.render('home', { users: users });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/users/create', function (req, res) {
   res.render('adduser');
 });
 
-app.post('/users/create', async (req, res) => {
+app.post('/users/create', function (req, res) {
   const name = req.body.name;
   const occupation = req.body.occupation;
   let newsletter = req.body.newsletter;
 
   if (newsletter === 'on') {
     newsletter = true;
-  } else {
-    newsletter = null;
   }
 
-  console.log(req.body);
-
-  await User.create({ name, occupation, newsletter });
-
-  res.redirect('/');
+  User.create({ name, occupation, newsletter })
+    .then(res.redirect('/'))
+    .catch((err) => console.log(err));
 });
 
-app.get('/', function (req, res) {
-  res.render('home');
+app.get('/users/:id', function (req, res) {
+  const id = req.params.id;
+
+  User.findOne({
+    raw: true,
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      console.log(user);
+      res.render('userview', { user });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post('/users/delete/:id', function (req, res) {
+  const id = req.params.id;
+
+  User.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/users/edit/:id', function (req, res) {
+  const id = req.params.id;
+
+  User.findOne({
+    raw: true,
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      console.log(user);
+      res.render('useredit', { user });
+    })
+    .catch((err) => console.log(err));
 });
 
 // Criar tabelas e rodar o app
